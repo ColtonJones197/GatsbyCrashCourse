91d4ae466a66fbef0a1a84dff49de664f439441d
Colton Jones <colton.jones197@gmail.com>
2020-08-17 15:10:53 -0500

    fixed createpage bug

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,7 +9,7 @@
 const path = require('path');
 
 exports.createPages = ({actions, graphql}) => {
-    const { createPage } = actions.createPage
+    const createPage = actions.createPage
 
     const postTemplate = path.resolve('src/templates/blog-post.js')
 
